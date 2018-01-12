7b23abad43d8f5b20e7f293138db12ef7020bbcf
AlgorithmsAreCool <AlgorithmsAreCool@gmail.com>
2018-01-12 16:44:06 -0600

    Add some basic performance testing

---
+++ ZstdNet.Benchmarks/CompressionBenchmarks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace ZstdNet.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class CompressionBenchmarks
+    {
+        byte[] Test = new byte[1024];
+        byte[] Dest = new byte[1024 * 2];
+
+        public CompressionBenchmarks()
+        {
+            var r = new Random(0);
+            r.NextBytes(Test);
+        }
+
+        Compressor ZNetCompressor = new Compressor(new CompressionOptions(1));
+
+        [Benchmark]
+        public void Compress1KBRandom()
+        {
+            ZNetCompressor.Wrap(Test, Dest, 0);
+        }
+    }
+}
